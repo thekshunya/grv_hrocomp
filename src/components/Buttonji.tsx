6e9ea26f9f5a268e002ed82c97eb9e518759eec8
thekshunya <shyamange12345@gmail.com>
2024-01-31 04:37:53 +0530

    first commit

---
+++ src/components/Buttonji.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button className=" shadow-gray-600 shadow-md  text-white text-sm font-medium leading-5 tracking-normal whitespace-nowrap justify-center items-stretch border bg-black  mb-5 w-40 h-10   rounded-xl border-solid border-black hover:bg-white hover:text-black transition duration-150">
+      Discover Our Plans
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Herocomp.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Buttonji from './Buttonji';
+import Image from 'next/image';
+const Herocomp = () => {
+  return (
+    <>
+      <div className="w-3/5 bg-slate-50 mx-auto  flex mt-28 justify-center items-center flex-col">
+        <div className="text-5xl font-bold mb-2 tracking-tighter trackin ">
+          Business. Simplified.
+        </div>
+        <p className="text-center font-normal px-12 mb-8 text-sm">
+          With Deskera ERP, automate and centralise business processes, gain
+          real-time insights and increase business control, increase data
+          accuracy, reduce costs, fulfill orders, and much more.
+        </p>
+        <Buttonji />
+      </div>
+      <div className="flex mt-10 justify-center">
+        <Image
+          src="/ig-erp-dashboard.webp"
+          width={1000}
+          height={1000}
+          alt="oy"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Herocomp;
